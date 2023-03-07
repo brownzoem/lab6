e51937759ed89e73a7740a8a29c7f8763f9aca30
brownzoem <brownzoem@gmail.com>
2023-03-07 16:14:13 -0500

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+def print_menu():
+    print("hi")
+
 def main():
-    print("helloooooooo")
-    #add comment
-    #add another comment
-
-
+    var = 8
+    var = 9
+    list = [1, 2, 3]
+    for x in list:
+        print_menu()
+        print(x)
+    var = 10
 
 if __name__ == "__main__":
     main()
